abcd020fcffd1101964c2bf37b784a5de65fc53f
nino <nino.faivre@netc.fr>
2022-06-28 21:10:18 +0200

    test

--- lua/Header42/init.lua
+++ lua/Header42/init.lua
@@ -1,12 +1,8 @@
-local M = {}
-M.setup = function ()
-	local main = require("Header42.srcs.main")
-	local cmd = vim.api.nvim_create_user_command
+local main = require("Header42.srcs.main")
+local cmd = vim.api.nvim_create_user_command
 
-	cmd('Header42',
-	function()
-		require("main").main()
-	end,
-	{desc = 'Header42'})
-end
-return M
+cmd('Header42',
+function()
+	main.main()
+end,
+{desc = 'Header42'})
