be5f99356d85af99a0a148b3bbf0831037ae8874
nino <nino.faivre@netc.fr>
2022-06-28 20:54:29 +0200

    add setup function

--- lua/Header42/init.lua
+++ lua/Header42/init.lua
@@ -1,8 +1,12 @@
-local main = require("Header42.srcs.main")
-local cmd = vim.api.nvim_create_user_command
+local M = {}
+M.setup = function ()
+	local main = require("Header42.srcs.main")
+	local cmd = vim.api.nvim_create_user_command
 
-cmd('Header42',
-function()
-	require("main").main()
-end,
-{desc = 'Header42'})
+	cmd('Header42',
+	function()
+		require("main").main()
+	end,
+	{desc = 'Header42'})
+end
+return M
